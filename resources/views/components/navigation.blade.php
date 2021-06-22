1efcd3a2564a89a161238208a88e54c91a177911
Jake Bogan <theronbogan1@gmail.com>
2021-06-22 17:09:28 -0400

    Medianet adds navbar

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -5,13 +5,50 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('/css/custom.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
 </head>
 <body>
-    <header><nav class="flex"><img src="{{ asset('/img/logo.svg') }}" /> Home | Movies | Games | News</nav></header>
+    
+    <header>
+        <nav class="flex justify-between nav-max-w mx-auto">
+            <ul class="flex items-center space-x-4">
+                <li class="">
+                    <a href="#" >
+                        <img src="{{ asset('/img/logo.svg') }}" />
+                    </a>
+                </li>
+                <li class="li-nav-border-color text-white border-r-2 pr-2">
+                    <a href="#">Home</a>
+                </li>
+                <li class="li-nav-border-color text-white border-r-2 pr-2">
+                    <a href="#">Movies</a>
+                </li>
+                <li class="li-nav-border-color text-white border-r-2 pr-2">
+                    <a href="#">Games</a>
+                </li>
+                <li class="li-nav-border-color text-white border-r-2 pr-2">
+                    <a href="#">News</a>
+                </li>
+            </ul>
+            
+            <div class="flex">
+                <input type="text">
+                <ul class="flex items-center">
+                    <li>
+                        <a href="#" class="bg-red-400">
+                            <img src="{{ asset('/img/person.svg') }}" class="inline-block"  alt="person icon"> Profile</li>
+                        </a>
+                    </ul>
+            </div>
+        </nav>
+    </header>
+    
+    {{-- <header><nav class="flex"><img src="{{ asset('/img/logo.svg') }}" /> Home | Movies | Games | News</nav></header> --}}
 
     {{$slot}}
 
-    <footer>Copyright</footer>
+    {{-- <footer>Copyright</footer> --}}
 </body>
 </html>
