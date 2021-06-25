39c9c4f3b12104641ed3bbb2162d8fa215bfc613
Josh Reynolds <82967109+reynoldsjoshua@users.noreply.github.com>
2021-06-25 15:22:02 -0500

    Medianet adds tailwindcss class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
     purge: [],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            fontSize: {
+                sm: ['14px', '20px'],
+            }
+        },
     },
     variants: {
         extend: {},
