6890b19b7449b7d0e7e5b6bcba7b81a90d3d861d
Jake Bogan <theronbogan1@gmail.com>
2021-06-28 14:24:20 -0400

    Medianet adds sass

--- public/js/app.js
+++ public/js/app.js
@@ -19100,10 +19100,10 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./resources/css/app.css":
-/*!*******************************!*\
-  !*** ./resources/css/app.css ***!
-  \*******************************/
+/***/ "./resources/css/tailwindcss.css":
+/*!***************************************!*\
+  !*** ./resources/css/tailwindcss.css ***!
+  \***************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
@@ -19417,7 +19417,7 @@ process.umask = function() { return 0; };
 /******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
 /******/ 		var installedChunks = {
 /******/ 			"/js/app": 0,
-/******/ 			"css/tailwindcss/app": 0,
+/******/ 			"css/tailwindcss": 0,
 /******/ 			"css/app": 0
 /******/ 		};
 /******/ 		
@@ -19466,9 +19466,9 @@ process.umask = function() { return 0; };
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["css/tailwindcss/app","css/app"], () => (__webpack_require__("./resources/js/app.js")))
-/******/ 	__webpack_require__.O(undefined, ["css/tailwindcss/app","css/app"], () => (__webpack_require__("./resources/sass/app.sass")))
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/tailwindcss/app","css/app"], () => (__webpack_require__("./resources/css/app.css")))
+/******/ 	__webpack_require__.O(undefined, ["css/tailwindcss","css/app"], () => (__webpack_require__("./resources/js/app.js")))
+/******/ 	__webpack_require__.O(undefined, ["css/tailwindcss","css/app"], () => (__webpack_require__("./resources/sass/app.sass")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/tailwindcss","css/app"], () => (__webpack_require__("./resources/css/tailwindcss.css")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -7,8 +7,8 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     {{-- <link rel="stylesheet" href="{{ asset('/css/custom.css') }}"> --}}
-    <link rel="stylesheet" href="{{ asset('/css/tailwindcss/app.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/tailwindcss.css') }}">
 </head>
 <body>
     
