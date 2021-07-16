2219640f312be18c72f457f9472cf3bd2ccf722b
Jake Bogan <theronbogan1@gmail.com>
2021-07-16 15:44:33 -0400

    Medianet adds photos

--- resources/views/components/index/game-news.blade.php
+++ resources/views/components/index/game-news.blade.php
@@ -8,8 +8,8 @@
                         <img src="{{ $news->url }}" alt="">
                     </div>
                     <div class="description">
-                        <h2>{{ substr($news->title, 0,  20) }}</h2>
-                        <p>{{ $news->description }}</p>
+                        <h2>{{ substr($news->title, 0,  17) }}&#8230;</h2>
+                        <p>{{ substr($news->description, 0,  57) }}&#8230;</p>
                     </div>
                 </div>
             @endif
